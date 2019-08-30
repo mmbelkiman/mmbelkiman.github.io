bdf0daadb2ad0f8abec6167aa0ccb2a70e2c0f10
Marcelo Belkiman <marcelobelkiman@gmail.com>
2019-08-29 21:59:41 -0300

    wip

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,6 +2,29 @@ $(document).ready(function () {
 	var displaySoftware = true;
 	var displayGames = true;
 	translate();
+	//$('.gif').gifplayer();
+	Gifffer({
+		playButtonStyles: {
+		  'width': '60px',
+		  'height': '60px',
+		  'border-radius': '30px',
+		  'background': 'rgba(255, 255, 255, 0.5)',
+		  'position': 'absolute',
+		  'top': '50%',
+		  'left': '50%',
+		  'margin': '-30px 0 0 -30px'
+		},
+		playButtonIconStyles: {
+		  'width': '0',
+		  'height': '0',
+		  'border-top': '14px solid transparent',
+		  'border-bottom': '14px solid transparent',
+		  'border-left': '14px solid rgba(0, 0, 0, 0.5)',
+		  'position': 'absolute',
+		  'left': '26px',
+		  'top': '16px'
+		}
+	  });
 
 	$(".filter-text").on("click", function () {
 		var id = $(this).attr("id");
